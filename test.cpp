e76602376eeef01facd5fca7e828fcfc8c88d0df
cions <cions@tuta.com>
2025-06-13 16:17:28 +0900

    fix

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include <atomic>
+#include <iostream>
+#include <stdexcept>
+#include <thread>
+#include <vector>
+
+int main() {
+	try {
+		throw std::runtime_error("exception");
+	} catch (const std::exception&) {
+		std::cout << "an exception was caught" << std::endl;
+	}
+
+	std::atomic<int> aint(1);
+
+	std::vector<std::thread> threads;
+	for (int i = 0; i < 16; ++i) {
+		threads.emplace_back([&]() {
+			int expected = aint.load(std::memory_order_seq_cst);
+			while (!aint.compare_exchange_strong(expected, expected * 2, std::memory_order_seq_cst));
+		});
+	}
+	for (auto && t : threads) {
+		t.join();
+	}
+
+	int expected = 65536;
+	int actual = aint.load(std::memory_order_seq_cst);
+	if (actual != expected) {
+		std::cout << "expected: " << expected << ", actual: " << actual << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
